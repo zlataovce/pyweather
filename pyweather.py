e5d5e7b02c7363cb9609f980e1646caadf01aa0e
zlataovce <34477304+zlataovce@users.noreply.github.com>
2020-05-24 18:37:09 +0200

    Update pyweather.py

--- pyweather.py
+++ pyweather.py
@@ -5,8 +5,8 @@ from bs4 import BeautifulSoup
 class today:
     def temp(url):
         r = requests.get(url, headers={'user-agent': 'Mozilla/5.0 (Macintosh; '
-                                                                   'Intel Mac OS X 10_12_6) AppleWebKit/537.36'
-                                                                   '(KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36'})
+                                       'Intel Mac OS X 10_12_6) AppleWebKit/537.36'
+                                       '(KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36'})
         soup = BeautifulSoup(r.content, "html.parser")
         temp = soup.find("div", {"class": "today_nowcard-temp"})
         temperaturestr = temp.text
@@ -14,8 +14,8 @@ class today:
 
     def phrase(url):
         r = requests.get(url, headers={'user-agent': 'Mozilla/5.0 (Macintosh; '
-                                                                   'Intel Mac OS X 10_12_6) AppleWebKit/537.36'
-                                                                   '(KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36'})
+                                       'Intel Mac OS X 10_12_6) AppleWebKit/537.36'
+                                       '(KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36'})
         soup = BeautifulSoup(r.content, "html.parser")
         phrase = soup.find("div", {"class": "today_nowcard-phrase"})
         phrasestr = phrase.text
