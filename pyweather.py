7f2f70ed3db54f1518d1ee8771a78006f7cd5957
zlataovce <34477304+zlataovce@users.noreply.github.com>
2020-05-24 17:37:01 +0200

    today

--- pyweather.py
+++ pyweather.py
@@ -1,12 +1,21 @@
 import requests
 from bs4 import BeautifulSoup
 
-url = 'https://weather.com/cs-CZ/pocasi/dnes/l/363d23dbd8c6451caca6c0ef93697c2aa8a47d574bd5b8f4ffbc15360a654d15'
-r = requests.get(url, headers={'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36'})
-soup = BeautifulSoup(r.content, "html.parser")
-temp = soup.find("div", {"class": "today_nowcard-temp"})
-phrase = soup.find("div", {"class": "today_nowcard-phrase"})
-
-temperaturestr = temp.text
-conditionstr = phrase.text
-weatherstr = temperaturestr + " " + conditionstr
+class today:
+  def temp(url):
+    r = requests.get(url, headers={'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36'})
+    soup = BeautifulSoup(r.content, "html.parser")
+    temp = soup.find("div", {"class": "today_nowcard-temp"})
+  
+    temperaturestr = temp.text
+    
+    return temperaturestr
+  
+  def phrase(url):
+    r = requests.get(url, headers={'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36'})
+    soup = BeautifulSoup(r.content, "html.parser")
+    phrase = soup.find("div", {"class": "today_nowcard-phrase"})
+  
+    phrasestr = temp.text
+    
+    return phrasestr
