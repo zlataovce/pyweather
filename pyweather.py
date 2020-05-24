3c08bdbaebf403c6e5bc5eb2d33340d416edf2e0
zlataovce <34477304+zlataovce@users.noreply.github.com>
2020-05-24 20:17:38 +0200

    added temp units converts

--- pyweather.py
+++ pyweather.py
@@ -3,15 +3,21 @@ from bs4 import BeautifulSoup
 
 
 class today:
-    def temp(url):
+    def temp(url, tmp):
         r = requests.get(url, headers={'user-agent': 'Mozilla/5.0 (Macintosh; '
                                        'Intel Mac OS X 10_12_6) AppleWebKit/53'
                                        '7.36(KHTML, like Gecko) Chrome/63.0.32'
                                        '39.132 Safari/537.36'})
         soup = BeautifulSoup(r.content, "html.parser")
         temp = soup.find("div", {"class": "today_nowcard-temp"})
-        temperaturestr = temp.text
-        return temperaturestr
+        temperaturestr = str(temp.text)
+        if tmp == "c":
+            withoutdegree = temperaturestr.replace("°", "")
+            temperaturewithoutdgr = (withoutdegree - 32) * 5.0/9.0
+            temperature = str(temperaturewithoutdgr) + "°"
+            return temperature
+        else:
+            return temperaturestr
 
     def phrase(url):
         r = requests.get(url, headers={'user-agent': 'Mozilla/5.0 (Macintosh; '
@@ -33,8 +39,14 @@ class tomorrow:
         soup = BeautifulSoup(r.content, "html.parser")
         daypart = soup.find("div", {"id": "daypart-1"})
         temp = daypart.find("div", {"class": "today-daypart-temp"})
-        temperaturestr = temp.text
-        return temperaturestr
+        temperaturestr = str(temp.text)
+        if tmp == "c":
+            withoutdegree = temperaturestr.replace("°", "")
+            temperaturewithoutdgr = (withoutdegree - 32) * 5.0/9.0
+            temperature = str(temperaturewithoutdgr) + "°"
+            return temperature
+        else:
+            return temperaturestr
 
     def phrase(url):
         r = requests.get(url, headers={'user-agent': 'Mozilla/5.0 (Macintosh; '
